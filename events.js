35954c105c9ad8ce4ae6873500bfaf29aa4cc5ea
giggityfiles <giggityfiles@gmail.com>
2025-10-09 19:54:47 -0400

    added ren and stimpy easter egg

--- events.js
+++ events.js
@@ -1,10 +1,27 @@
-const today = new Date()
-const month = today.getMonth() + 1
-const day = today.getDate()
-const year = today.getFullYear()
+const today = new Date();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+const year = today.getFullYear();
 
 if (month === 10 && day === 9 && year >= 2025) {
-  alert("happy birthday youtomb mobile")
-} else if (month === 1 && day === 1) {
-  alert("happy new year")
+  alert("happy birthday youtomb mobile");
+
+  const headline = document.querySelector(".compact-media-headline");
+  if (headline) {
+    headline.textContent = "HAPPY BIRTHDAY YOUTOMB MOBILE!!!";
+  }
+
+  if (document.querySelector(".style-2013")) {
+    const smallText = document.querySelector(".small-text");
+    if (smallText) {
+      smallText.textContent = "BIRTHDAY PARTY WHAR";
+    }
+  }
+
+  const imgs = document.querySelectorAll("img");
+  imgs.forEach(img => {
+    if (Math.random() < 1 / 500) {
+      img.src = "https://youtomb-mobile.github.io/stk.webp";
+    }
+  });
 }
