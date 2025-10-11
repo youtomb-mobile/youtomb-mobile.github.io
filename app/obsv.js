a25d409a4446c7d7e196128c9594b18b18f6a45d
giggityfiles <giggityfiles@gmail.com>
2025-10-11 14:19:33 -0400

    octelodeon

--- app/obsv.js
+++ app/obsv.js
@@ -20,3 +20,50 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   blockcheck.observe(document.body, { childList: true, subtree: true, characterData: true });
 });
+
+const forbidden2 = ["67", "sprunki"];
+const replacement2 = "brainrot video";
+
+function scanAndReplace() {
+  document.querySelectorAll("*").forEach(el => {
+    if (el.children.length === 0 && typeof el.innerText === "string") {
+      const text = el.innerText;
+      if (forbidden.some(word => text.toLowerCase().includes(word.toLowerCase()))) {
+        el.innerText = replacement;
+        el.style.fontFamily = "Roboto Condensed";
+      }
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  scanAndReplace();
+  const gog = new MutationObserver(() => {
+    setTimeout(scanAndReplace, 500);
+  });
+  gog.observe(document.body, { childList: true, subtree: true, characterData: true });
+});
+
+const forbidden3 = ["PewDiePie", "Markiplier", "Yo Mama"];
+const replacement3 = "Popular Youtomber";
+
+function scanAndReplace() {
+  document.querySelectorAll("*").forEach(el => {
+    if (el.children.length === 0 && typeof el.innerText === "string") {
+      const text = el.innerText;
+      if (forbidden.some(word => text.toLowerCase().includes(word.toLowerCase()))) {
+        el.innerText = replacement;
+        el.style.fontFamily = "Arial Narrow";
+      }
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  scanAndReplace();
+  const gog = new MutationObserver(() => {
+    setTimeout(scanAndReplace, 500);
+  });
+  gog.observe(document.body, { childList: true, subtree: true, characterData: true });
+});
+
