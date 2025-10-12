171148418b6a8520febb6fe5186347bf79ace367
giggityfiles <giggityfiles@gmail.com>
2025-10-12 14:51:12 -0400

    ses

--- app/obsv.js
+++ app/obsv.js
@@ -1,69 +1,150 @@
-const forbidden = ["ren", "pokemon", "trump", "news", "chicken stars"];
-const replacement = "Æ BLOCK YOUTOMB GIGGITY GOOGLE";
-
-function scanAndReplace() {
-  document.querySelectorAll("*").forEach(el => {
-    if (el.children.length === 0 && typeof el.innerText === "string") {
-      const text = el.innerText;
-      if (forbidden.some(word => text.toLowerCase().includes(word.toLowerCase()))) {
-        el.innerText = replacement;
-        el.style.fontFamily = "BBH Sans Bartle";
-      }
-    }
-  });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  scanAndReplace();
-  const blockcheck = new MutationObserver(() => {
-    setTimeout(scanAndReplace, 500);
-  });
-  blockcheck.observe(document.body, { childList: true, subtree: true, characterData: true });
-});
-
-const forbidden2 = ["67", "sprunki", "klasky", "csupo", "robot logo", "logo"];
-const replacement2 = "brainrot video";
-
-function scanAndReplace() {
-  document.querySelectorAll("*").forEach(el => {
-    if (el.children.length === 0 && typeof el.innerText === "string") {
-      const text = el.innerText;
-      if (forbidden.some(word => text.toLowerCase().includes(word.toLowerCase()))) {
-        el.innerText = replacement;
-        el.style.fontFamily = "Roboto Condensed";
-      }
-    }
-  });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  scanAndReplace();
-  const gog = new MutationObserver(() => {
-    setTimeout(scanAndReplace, 500);
-  });
-  gog.observe(document.body, { childList: true, subtree: true, characterData: true });
-});
-
-const forbidden3 = ["pewdiepie", "markiplier", "yo mama"];
-const replacement3 = "Popular Youtomber";
-
-function scanAndReplace() {
-  document.querySelectorAll("*").forEach(el => {
-    if (el.children.length === 0 && typeof el.innerText === "string") {
-      const text = el.innerText;
-      if (forbidden.some(word => text.toLowerCase().includes(word.toLowerCase()))) {
-        el.innerText = replacement;
-        el.style.fontFamily = "Arial Narrow";
-      }
-    }
-  });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  scanAndReplace();
-  const gog = new MutationObserver(() => {
-    setTimeout(scanAndReplace, 500);
-  });
-  gog.observe(document.body, { childList: true, subtree: true, characterData: true });
-});
-
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("i shit my pants");
+console.log("spam ending now");
