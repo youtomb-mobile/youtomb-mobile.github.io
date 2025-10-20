93d117d9ca7ceceb7ff29c9cb198b6bc82332707
giggityfiles <giggityfiles@gmail.com>
2025-10-19 20:31:38 -0400

    wharmode fix

--- ford.js
+++ ford.js
@@ -4,16 +4,7 @@
     document.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
       if (link.href.includes("2015YTm.css")) {
         link.href = link.href.replace("2015YTm.css", "jrod.css");
-      }
-    });
-  }
-})();
-
-(() => {
-  const params = new URLSearchParams(window.location.search);
-  if (params.has("wharmode")) {
-    document.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
-      if (link.href.includes("2015YTm.css")) {
+      } else if (link.href.includes("player.css")) {
         link.href = link.href.replace("player.css", "griddy.css");
       }
     });
