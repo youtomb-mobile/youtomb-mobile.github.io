f4190990d596e8f2794bebd33d1301bc267ec94d
giggityfiles <giggityfiles@gmail.com>
2025-10-19 20:27:15 -0400

    jrod update

---
+++ ford.js
@@ -0,0 +1,21 @@
+(() => {
+  const params = new URLSearchParams(window.location.search);
+  if (params.has("wharmode")) {
+    document.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
+      if (link.href.includes("2015YTm.css")) {
+        link.href = link.href.replace("2015YTm.css", "jrod.css");
+      }
+    });
+  }
+})();
+
+(() => {
+  const params = new URLSearchParams(window.location.search);
+  if (params.has("wharmode")) {
+    document.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
+      if (link.href.includes("2015YTm.css")) {
+        link.href = link.href.replace("player.css", "griddy.css");
+      }
+    });
+  }
+})();
